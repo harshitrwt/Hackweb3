6e43f17bf5388f3eaf918047d1eb1d1e25c74dc2
Harshit Rawat <143377593+harshitrwt@users.noreply.github.com>
2025-04-27 16:24:24 +0530

    final_changes

--- components/NFTBussinessCard.tsx
+++ components/NFTBussinessCard.tsx
@@ -4,36 +4,30 @@ import { BUSINESSES_CONTRACT_ADDRESS, STAKING_CONTRACT_ADDRESS } from "../consta
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
 
-// Props for the NFTCard component
-// NFT to display in shop
+
 type Props = {
     nft: NFT;
 };
 
 export default function NFTCard({ nft }: Props) {
-    // Get the user's address
-    // Pass the address to the handle claim function to set approval for the staking contract
+
     const address = useAddress();
 
-    // Get the businesses contract instance
-    // Get the claim conditions for the NFT for cost data
+    
     const { contract: businessesContract } = useContract(BUSINESSES_CONTRACT_ADDRESS, "edition-drop");
     const { data: claimCondition } = useClaimConditions(businessesContract, nft.metadata.id);
 
     // Get the staking contract instance
     const { contract: stakingContract } = useContract(STAKING_CONTRACT_ADDRESS);
 
-    // Function to calculate the earnings per hour for each business NFT
     const calculateEarnings = (cost: number) => {
         return cost * 0.1;
     };
 
-    // State for the claim state
+    
     const [claimState, setClaimState] = useState<
     "init" | "nftClaim" | "staking">("init");
 
-    // Function to handle the claim of the NFT
-    // Claims business NFT and stakes it for the user
     const handleClaim = async () => {
         if(!address){
             return;
