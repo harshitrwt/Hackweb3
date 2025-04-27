e1aea46e5cc3ebcf92fd7b7e781d54297638c883
Harshit Rawat <143377593+harshitrwt@users.noreply.github.com>
2025-04-27 16:27:19 +0530

    final_changes

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,11 +6,11 @@ import { getUser } from "../pages/api/auth/[...thirdweb]";
 import { WORKER_CONTRACT_ADDRESS } from "../constants/contracts";
 
 
-// Set loginOptional to false to require the user to login
+
 const loginOptional = false;
 
 const Login = () => {
-    // Return the ConnectEmbed component if the user is not logged in
+
     const showConnectEmbed = useShowConnectEmbed();
 
     
@@ -18,34 +18,33 @@ const Login = () => {
     const address = useAddress();
     const sdk = useSDK();
 
-    // Check if the user is logged in and redirect to the home page if they are
+
     const { isLoggedIn, isLoading } = useUser();
     const router = useRouter();
 
-    // States for loading worker status
+
     const [loadingWorkerStatus, setLoadingWorkerStatus] = useState(false);
     const [loadingStatus, setLoadingStatus] = useState("");
 
-    // Function to check if the user is a new player and claim a worker and tokens
-    // If not a new player, redirect to the home page
+
     const checkNewPlayer = async () => {
         try {
-            // Check if the wallet is a SmartWallet and the user's address and SDK are available
+         
             if(wallet instanceof SmartWallet && address && sdk) {
-                // Set the loading status to true and status message
+            
                 setLoadingWorkerStatus(true);
                 setLoadingStatus("Checking worker balance...");
 
-                // Check if the user has a worker NFT
+              
                 const workerContract = await sdk?.getContract(WORKER_CONTRACT_ADDRESS);
                 const workerBalance = await workerContract?.erc721.balanceOf(address);
 
-                // If the user does not have a worker, claim a worker and tokens
+                
                 if(workerBalance?.toNumber() === 0) {
-                    // Set the status message
+     
                     setLoadingStatus("No worker found...");
                     try {
-                        // Set the status message and claim the worker and tokens with Engine
+               
                         setLoadingStatus("Claiming worker and tokens...")
                         const response = await fetch("/api/claimToken", {
                             method: "POST",
@@ -60,18 +59,18 @@ const Login = () => {
                             throw new Error(data.message);
                         }
 
-                        // Set the status message
+                        
                         setLoadingStatus("Worker and tokens claimed...");
                     } catch (error) {
                         console.error(error);
                         alert("Error creating new account. Please try again.");
                     } finally {
-                        // Redirect to the home page
+                        
                         setLoadingStatus("");
                         router.push("/");
                     }
                 } else {
-                    // If the user has a worker, redirect to the home page
+                  
                     setLoadingStatus("");
                     router.push("/");
                 }
@@ -84,14 +83,14 @@ const Login = () => {
         }
     };
 
-    // Run the checkNewPlayer function when the user is logged in and not loading
+  
     useEffect(() => {
         if(isLoggedIn && !isLoading) {
             checkNewPlayer();
         }
     }, [isLoggedIn, isLoading, wallet]);
 
-    // Return status message if claiming worker and tokens
+   
     if(loadingWorkerStatus) {
         return (
             <div className={styles.container}>
@@ -116,7 +115,7 @@ const Login = () => {
 
 export default Login;
 
-// This is a server-side function that checks if the user is logged in and redirects to the home page if not.
+
 export async function getServerSideProps(context: any) {
     const user = await getUser(context.req);
 
